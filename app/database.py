3bbfc5a80860b196cafc10fb8961b3b5efe5c1b8
mchudinow <118258151+mchudinow@users.noreply.github.com>
2026-03-08 20:42:28 +0300

    fix database connection for render

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:postgres@db:5432/shortener"
+DATABASE_URL = "postgresql://postgres221:4N40vLiXdki9sQ2GeSisTBe7MDW4INKS@dpg-d6mr70ntskes73e3da20-a.oregon-postgres.render.com/shortener_n5dh"
 
 engine = create_engine(DATABASE_URL)
 
